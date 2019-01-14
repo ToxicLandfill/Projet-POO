f91f0d17e88e6bdd182588e24dc321bb6c8258af
Paul <paul.bruneau.etu@univ-lemans.fr>
2019-01-14 16:28:50 +0100

    ajout classes Championnat et Coupe

---
+++ Projet POO/src/main/Championnat.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Championnat {
+
+	public Championnat() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Projet POO/src/main/Coupe.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Coupe extends Tournoi {
+
+	public Coupe() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
